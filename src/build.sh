273f96aad4dc6c1c9af3c30b826848114ed2f9c0
Philip Harney <theevillime@gmail.com>
2017-01-17 16:28:44 +0000

    Update build script to include font licenses

--- src/build.sh
+++ src/build.sh
@@ -16,6 +16,8 @@ lessc "src/css/print.less" "_assets/ebook/sushi.css";
 mkdir -p _assets/website/fonts/lato;
 cp -R bower_components/lato/font/ _assets/website/fonts/lato/;
 
+cp bower_components/lato/README.md _assets/website/fonts/lato;
+
 mkdir -p _assets/ebook/fonts/lato;
 cp -R bower_components/lato/font/ _assets/ebook/fonts/lato/;
 
@@ -23,5 +25,9 @@ cp -R bower_components/lato/font/ _assets/ebook/fonts/lato/;
 mkdir -p _assets/website/fonts/league-gothic;
 cp -R bower_components/league-gothic/webfonts/leaguegothic-regular* _assets/website/fonts/league-gothic/;
 
+cp bower_components/league-gothic/*.markdown _assets/website/fonts/league-gothic/;
+
 mkdir -p _assets/ebook/fonts/league-gothic;
 cp -R bower_components/league-gothic/webfonts/leaguegothic-regular* _assets/ebook/fonts/league-gothic/;
+
+cp bower_components/league-gothic/*.markdown _assets/ebook/fonts/league-gothic/;

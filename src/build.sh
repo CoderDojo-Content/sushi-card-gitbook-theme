228954509845b8014e230cddada6df01b1cab2d8
Philip Harney <theevillime@gmail.com>
2017-01-17 16:08:40 +0000

    Ignore built files (& add ebook fonts)

--- src/build.sh
+++ src/build.sh
@@ -16,6 +16,12 @@ lessc "src/css/print.less" "_assets/ebook/sushi.css";
 mkdir -p _assets/website/fonts/lato;
 cp -R bower_components/lato/font/ _assets/website/fonts/lato/;
 
+mkdir -p _assets/ebook/fonts/lato;
+cp -R bower_components/lato/font/ _assets/ebook/fonts/lato/;
+
 # League Gothic
 mkdir -p _assets/website/fonts/league-gothic;
 cp -R bower_components/league-gothic/webfonts/leaguegothic-regular* _assets/website/fonts/league-gothic/;
+
+mkdir -p _assets/ebook/fonts/league-gothic;
+cp -R bower_components/league-gothic/webfonts/leaguegothic-regular* _assets/ebook/fonts/league-gothic/;
